d8929e88ce97a45972a3729d9fe67a83e4519c45
Rico Tan <trickshotgaming19@gmail.com>
2026-05-10 00:52:35 +0800

    Activity 2 update

--- Assets/Scripts/MultiplayerMenu.cs
+++ Assets/Scripts/MultiplayerMenu.cs
@@ -1,17 +1,32 @@
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
 
 public class MultiplayerMenu : MonoBehaviour
 {
+    public TMP_Text playerCountText;
+    public GameObject[] playerCountObjects;
+    public int playerCount;
+    public GameObject menuUI;
+
+    void Update() {
+        playerCountObjects = GameObject.FindGameObjectsWithTag("Player");
+        playerCount = playerCountObjects.Length;
+        playerCountText.text = "Players: " + playerCount;
+    }
+
     public void StartHost() {
         NetworkManager.Singleton.StartHost();
+        menuUI.SetActive(false);
     }
 
     public void StartClient() {
         NetworkManager.Singleton.StartClient();
+        menuUI.SetActive(false);
     }
 
     public void StartServer() {
         NetworkManager.Singleton.StartServer();
+        menuUI.SetActive(false);
     }
 }
